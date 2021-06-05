8de84a8a729483136fb2af252d99a76e96403a82
Ivan Ivanov <ii99xt1@gmail.com>
2021-06-05 19:45:08 +0300

    create db.properties file and Spring Class DbConfig

---
+++ src/main/java/ua/com/foxminded/university/springconfig/DbConfig.java
@@ -0,0 +1,38 @@
+package ua.com.foxminded.university.springconfig;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+public class DbConfig {
+    private static final String DRIVER = "db.driver";
+    private static final String URL = "db.url";
+    private static final String LOGIN = "db.login";
+    private static final String PASSWORD = "db.password";
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty(DRIVER));
+        dataSource.setUrl(env.getRequiredProperty(URL));
+        dataSource.setUsername(env.getRequiredProperty(LOGIN));
+        dataSource.setPassword(env.getRequiredProperty(PASSWORD));
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
