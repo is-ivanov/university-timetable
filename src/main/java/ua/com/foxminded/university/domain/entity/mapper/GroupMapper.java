99175728ca0399af7a24303ca9bd2569143e1b67
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2021-06-08 13:55:35 +0300

    create GroupMapper

--- src/main/java/ua/com/foxminded/university/domain/entity/mapper/GroupMapper.java
+++ src/main/java/ua/com/foxminded/university/domain/entity/mapper/GroupMapper.java
@@ -6,30 +6,40 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 
 import ua.com.foxminded.university.domain.entity.Faculty;
+import ua.com.foxminded.university.domain.entity.Group;
 import ua.com.foxminded.university.domain.entity.Teacher;
 
-public class FacultyMapper implements RowMapper<Faculty> {
+public class GroupMapper implements RowMapper<Group> {
 
     private static final String ID = "id";
-    private static final String NAME = "name";
-    private static final String ID_DEAN = "dean_id";
+    private static final String GROUP_NAME = "group_name";
+    private static final String FACULTY_ID = "faculty_id";
+    private static final String FACULTY_NAME = "faculty_name";
+    private static final String DEAN_ID = "dean_id";
     private static final String FIRST_NAME_DEAN = "first_name";
     private static final String PATRONYMIC_DEAN = "patronymic";
     private static final String LAST_NAME_DEAN = "last_name";
 
     @Override
-    public Faculty mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Faculty faculty = new Faculty();
-        faculty.setId(rs.getInt(ID));
-        faculty.setName(rs.getString(NAME));
+    public Group mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Group group = new Group();
+        group.setId(rs.getInt(ID));
+        group.setName(rs.getString(GROUP_NAME));
+        group.setFaculty(createFaculty(rs));
+        return group;
+    }
 
+    private Faculty createFaculty(ResultSet rs) throws SQLException {
+        Faculty faculty = new Faculty();
+        faculty.setId(rs.getInt(FACULTY_ID));
+        faculty.setName(rs.getString(FACULTY_NAME));
         faculty.setDean(createDean(rs));
         return faculty;
     }
 
     private Teacher createDean(ResultSet rs) throws SQLException {
         Teacher dean = new Teacher();
-        Integer id = rs.getInt(ID_DEAN);
+        Integer id = rs.getInt(DEAN_ID);
         if (rs.wasNull()) {
             id = null;
         }
@@ -39,5 +49,4 @@ public class FacultyMapper implements RowMapper<Faculty> {
         dean.setLastName(rs.getString(LAST_NAME_DEAN));
         return dean;
     }
-
 }
