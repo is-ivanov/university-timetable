5f520ab5bb57a5bf0ba070b2d283400bef11e825
Ivan Ivanou <ii99xt1@gmail.com>
2021-06-17 00:00:31 +0300

    create CourseMapperTest

---
+++ src/test/java/ua/com/foxminded/university/domain/entity/mapper/CourseMapperTest.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.university.domain.entity.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CourseMapperTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
