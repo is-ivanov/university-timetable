0d1af4819ea894139e452707190ff4c7571feae8
Ivan Ivanov <ii99xt1@gmail.com>
2021-06-17 20:52:00 +0300

    create RoomMapperTest, StudentMapperTest and TeacherMapperTest

---
+++ src/test/java/ua/com/foxminded/university/domain/entity/mapper/RoomMapperTest.java
@@ -0,0 +1,53 @@
+package ua.com.foxminded.university.domain.entity.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import ua.com.foxminded.university.domain.entity.Room;
+
+@ExtendWith(MockitoExtension.class)
+class RoomMapperTest {
+
+    private static final String ID = "id";
+    private static final String NUMBER = "number";
+    private static final String BUILDING = "building";
+    private static final int EXPECTED_ID = 1;
+    private static final String EXPECTED_ROOM_NUMBER = "111b";
+    private static final String EXPECTED_BUILDING = "test building";
+    private static final int ROW_NUM = 1;
+
+    private RoomMapper mapper;
+
+    @Mock
+    private ResultSet resultSetMock;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        mapper = new RoomMapper();
+    }
+
+    @Test
+    @DisplayName("test mapRow should return expected Room")
+    void testMapRowShouldReturnExpectedRoom() throws SQLException {
+        Room expectedRoom = new Room(EXPECTED_ID, EXPECTED_BUILDING,
+                EXPECTED_ROOM_NUMBER);
+        when(resultSetMock.getInt(ID)).thenReturn(EXPECTED_ID);
+        when(resultSetMock.getString(BUILDING)).thenReturn(EXPECTED_BUILDING);
+        when(resultSetMock.getString(NUMBER)).thenReturn(EXPECTED_ROOM_NUMBER);
+
+        Room actualRoom = mapper.mapRow(resultSetMock, ROW_NUM);
+
+        assertEquals(expectedRoom, actualRoom);
+    }
+
+}
---
+++ src/test/java/ua/com/foxminded/university/domain/entity/mapper/StudentMapperTest.java
@@ -0,0 +1,66 @@
+package ua.com.foxminded.university.domain.entity.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import ua.com.foxminded.university.domain.entity.Faculty;
+import ua.com.foxminded.university.domain.entity.Group;
+import ua.com.foxminded.university.domain.entity.Student;
+
+@ExtendWith(MockitoExtension.class)
+class StudentMapperTest {
+
+    private static final String ID = "student_id";
+    private static final String FIRST_NAME = "first_name";
+    private static final String LAST_NAME = "last_name";
+    private static final String PATRONYMIC = "patronymic";
+    private static final int EXPECTED_ID = 1;
+    private static final String EXPECTED_FIRST_NAME = "Ivan";
+    private static final String EXPECTED_LAST_NAME = "Ivanov";
+    private static final String EXPECTED_PATRONYMIC = "Ivanovich";
+    private static final int ROW_NUM = 1;
+
+    private StudentMapper mapper;
+
+    @Mock
+    private ResultSet resultSetMock;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        mapper = new StudentMapper();
+    }
+
+    @Test
+    @DisplayName("test mapRow should return expected Student")
+    void testMapRowShouldReturnExpectedStudent() throws SQLException {
+        Faculty expectedFaculty = new Faculty();
+        Group expectedGroup = new Group();
+        expectedGroup.setFaculty(expectedFaculty);
+        Student expectedStudent = new Student();
+        expectedStudent.setId(EXPECTED_ID);
+        expectedStudent.setFirstName(EXPECTED_FIRST_NAME);
+        expectedStudent.setLastName(EXPECTED_LAST_NAME);
+        expectedStudent.setPatronymic(EXPECTED_PATRONYMIC);
+        expectedStudent.setGroup(expectedGroup);
+
+        when(resultSetMock.getInt(ID)).thenReturn(EXPECTED_ID);
+        when(resultSetMock.getString(FIRST_NAME))
+                .thenReturn(EXPECTED_FIRST_NAME);
+        when(resultSetMock.getString(LAST_NAME)).thenReturn(EXPECTED_LAST_NAME);
+        when(resultSetMock.getString(PATRONYMIC))
+                .thenReturn(EXPECTED_PATRONYMIC);
+        Student actualStudent = mapper.mapRow(resultSetMock, ROW_NUM);
+        assertEquals(expectedStudent, actualStudent);
+    }
+
+}
