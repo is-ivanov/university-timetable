1be50b7a219df7fe3c16151221fc89cb5457682e
Ivan Ivanov <ii99xt1@gmail.com>
2021-06-17 23:23:00 +0300

    change LessonExtractorTest

--- src/test/java/ua/com/foxminded/university/domain/entity/mapper/LessonExtractorTest.java
+++ src/test/java/ua/com/foxminded/university/domain/entity/mapper/LessonExtractorTest.java
@@ -1,20 +1,55 @@
 package ua.com.foxminded.university.domain.entity.mapper;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import ua.com.foxminded.university.domain.entity.Lesson;
+import ua.com.foxminded.university.domain.entity.Student;
+
+@ExtendWith(MockitoExtension.class)
 class LessonExtractorTest {
+
+    private final int ROW_NUM = 1;
+
+    private LessonExtractor lessonExtractor;
+
+    @Mock
+    private ResultSet resultSetMock;
+
+    @Mock
+    private LessonMapper lessonMapper;
+
+    @Mock
+    private StudentMapper studentMapper;
 
     @BeforeEach
     void setUp() throws Exception {
+        lessonExtractor = new LessonExtractor(lessonMapper, studentMapper);
     }
 
     @Test
-    void test() {
-        // TODO
-        fail("Not yet implemented");
+    @DisplayName("when RS.next return one true then should call LessonMapper and StudentMapper once")
+    void testNumberCallsLessonMapperStudentMapper() throws SQLException {
+        when(resultSetMock.next()).thenReturn(true).thenReturn(false);
+        Lesson lesson = new Lesson();
+        when(lessonMapper.mapRow(resultSetMock, ROW_NUM)).thenReturn(lesson);
+        Student student = new Student();
+        when(studentMapper.mapRow(resultSetMock, ROW_NUM)).thenReturn(student);
+        lessonExtractor.extractData(resultSetMock);
+        verify(lessonMapper, times(1)).mapRow(resultSetMock, ROW_NUM);
+        verify(studentMapper, times(1)).mapRow(resultSetMock, ROW_NUM);
+
     }
 
 }
--- src/test/java/ua/com/foxminded/university/domain/entity/mapper/LessonMapperTest.java
+++ src/test/java/ua/com/foxminded/university/domain/entity/mapper/LessonMapperTest.java
@@ -1,11 +1,11 @@
 package ua.com.foxminded.university.domain.entity.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -18,6 +18,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import ua.com.foxminded.university.domain.entity.Course;
+import ua.com.foxminded.university.domain.entity.Department;
+import ua.com.foxminded.university.domain.entity.Faculty;
 import ua.com.foxminded.university.domain.entity.Lesson;
 import ua.com.foxminded.university.domain.entity.Room;
 import ua.com.foxminded.university.domain.entity.Student;
@@ -29,9 +31,10 @@ class LessonMapperTest {
     private static final String PATTERN_DATE_TIME = "yyyy-MM-dd HH:mm:ss";
     private static final String TIME_START = "time_start";
     private static final String TIME_END = "time_end";
-    private static final String EXPECTED_TIME_START = "2021-05-14 14:00:00";
-    private static final String EXPECTED_TIME_END = "2021-05-14 15:00:00";
+    private static final String EXPECTED_STRING_TIME_START = "2021-05-14 14:00:00";
+    private static final String EXPECTED_STRING_TIME_END = "2021-05-14 15:00:00";
     private static final String ID = "id";
+    private static final String TEACHER_ID = "teacher_id";
     private static final int EXPECTED_ID = 1;
     private static final int ROW_NUM = 1;
 
@@ -49,25 +52,34 @@ class LessonMapperTest {
     @DisplayName("test mapRow should return expected Lesson with empty List<Student>")
     void testMapRowShouldReturnLessonWithoutStudents() throws SQLException {
         Teacher teacher = new Teacher();
+        teacher.setId(EXPECTED_ID);
+        Department department = new Department();
+        Faculty faculty = new Faculty();
+        department.setFaculty(faculty);
+        teacher.setDepartment(department);
         Course course = new Course();
         Room room = new Room();
         Lesson expectedLesson = new Lesson();
+        expectedLesson.setId(EXPECTED_ID);
         expectedLesson.setTeacher(teacher);
         expectedLesson.setCourse(course);
         expectedLesson.setRoom(room);
         DateTimeFormatter formatter = DateTimeFormatter
                 .ofPattern(PATTERN_DATE_TIME);
-        expectedLesson.setTimeStart(
-                LocalDateTime.parse(EXPECTED_TIME_START, formatter));
-        expectedLesson
-                .setTimeEnd(LocalDateTime.parse(EXPECTED_TIME_END, formatter));
+        LocalDateTime expectedTimeStart = LocalDateTime
+                .parse(EXPECTED_STRING_TIME_START, formatter);
+        LocalDateTime expectedTimeEnd = LocalDateTime
+                .parse(EXPECTED_STRING_TIME_END, formatter);
+        expectedLesson.setTimeStart(expectedTimeStart);
+        expectedLesson.setTimeEnd(expectedTimeEnd);
         expectedLesson.setStudents(new ArrayList<Student>());
 
-        doReturn(EXPECTED_TIME_START).when(resultSetMock).getString(TIME_START);
+        when(resultSetMock.getInt(TEACHER_ID)).thenReturn(EXPECTED_ID);
         when(resultSetMock.getInt(ID)).thenReturn(EXPECTED_ID);
-//        when(resultSetMock.getString(TIME_START))
-//                .thenReturn(EXPECTED_TIME_START);
-        when(resultSetMock.getString(TIME_END)).thenReturn(EXPECTED_TIME_END);
+        when(resultSetMock.getTimestamp(TIME_START))
+                .thenReturn(Timestamp.valueOf(expectedTimeStart));
+        when(resultSetMock.getTimestamp(TIME_END))
+                .thenReturn(Timestamp.valueOf(expectedTimeEnd));
 
         Lesson actualLesson = mapper.mapRow(resultSetMock, ROW_NUM);
         assertEquals(expectedLesson, actualLesson);
