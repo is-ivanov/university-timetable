0653cf79177a354c20ef08cec99a4e10b0b71fd6
Ivan Ivanov <ii99xt1@gmail.com>
2021-07-26 10:10:38 +0300

    modify heater and footer. Add sort into table in faculty.html

--- src/main/java/ua/com/foxminded/university/controller/GroupController.java
+++ src/main/java/ua/com/foxminded/university/controller/GroupController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.com.foxminded.university.domain.service.interfaces.FacultyService;
 import ua.com.foxminded.university.domain.service.interfaces.GroupService;
 
 @Controller
@@ -12,15 +13,18 @@ import ua.com.foxminded.university.domain.service.interfaces.GroupService;
 public class GroupController {
 
     private final GroupService groupService;
+    private final FacultyService facultyService;
 
     @Autowired
-    public GroupController(GroupService groupService) {
+    public GroupController(GroupService groupService, FacultyService facultyService) {
         this.groupService = groupService;
+        this.facultyService = facultyService;
     }
 
     @GetMapping
     public String groups(Model model) {
         model.addAttribute("groups", groupService.getAll());
+        model.addAttribute("faculties",facultyService.getAll());
         return "group";
     }
 }
