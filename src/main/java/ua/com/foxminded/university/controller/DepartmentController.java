942f9dc4b4c56b22418e983354a9f596319e29f4
Ivan Ivanou <ii99xt1@gmail.com>
2021-07-30 13:07:52 +0300

    change method into DepartmentController and change department.html

--- src/main/java/ua/com/foxminded/university/controller/DepartmentController.java
+++ src/main/java/ua/com/foxminded/university/controller/DepartmentController.java
@@ -6,45 +6,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ua.com.foxminded.university.domain.entity.Department;
 import ua.com.foxminded.university.domain.entity.Faculty;
-import ua.com.foxminded.university.domain.entity.Group;
+import ua.com.foxminded.university.domain.service.interfaces.DepartmentService;
 import ua.com.foxminded.university.domain.service.interfaces.FacultyService;
-import ua.com.foxminded.university.domain.service.interfaces.GroupService;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/group")
-public class GroupController {
+@RequestMapping("/department")
+public class DepartmentController {
 
-    private final GroupService groupService;
+    private final DepartmentService departmentService;
     private final FacultyService facultyService;
 
     @Autowired
-    public GroupController(GroupService groupService, FacultyService facultyService) {
-        this.groupService = groupService;
+    public DepartmentController(DepartmentService groupService,
+                                FacultyService facultyService) {
+        this.departmentService = groupService;
         this.facultyService = facultyService;
     }
 
     @GetMapping
     public String groups(@RequestParam(value = "facultyId", required = false) Integer facultyId,
-                         @RequestParam(value = "isShowInactive", required =
-                             false) String isShowInactive,
                          Model model) {
-        if (isShowInactive != null && isShowInactive.equals("on")) {
-            model.addAttribute("isShowInactive", true);
-        }
         List<Faculty> allFaculties = facultyService.getAllSortedAscByName();
         model.addAttribute("faculties", allFaculties);
         Faculty facultySelected = null;
         if (facultyId != null) {
-            List<Group> groups = groupService.getAllByFacultyId(facultyId);
-            model.addAttribute("groups", groups);
+            List<Department> departments = departmentService.getAllByFacultyId(facultyId);
+            model.addAttribute("departments", departments);
             facultySelected = allFaculties.stream()
                 .filter(faculty -> faculty.getId() == facultyId)
                 .findFirst().orElse(null);
         }
         model.addAttribute("facultySelected", facultySelected);
-        return "group";
+        return "department";
     }
 }
