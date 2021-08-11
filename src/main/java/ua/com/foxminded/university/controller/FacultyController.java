d498d07b792304f4ec725fc2331d90f2ee1ab227
Ivan Ivanou <ii99xt1@gmail.com>
2021-08-12 00:17:44 +0300

    add logger and @RequiredArgsConstructor in FacultyController

--- src/main/java/ua/com/foxminded/university/controller/FacultyController.java
+++ src/main/java/ua/com/foxminded/university/controller/FacultyController.java
@@ -1,26 +1,26 @@
 package ua.com.foxminded.university.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ua.com.foxminded.university.domain.service.interfaces.FacultyService;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/faculty")
 public class FacultyController {
 
     private final FacultyService facultyService;
 
-    @Autowired
-    public FacultyController(FacultyService facultyService) {
-        this.facultyService = facultyService;
-    }
-
     @GetMapping
     public String faculties(Model model) {
+        log.debug("Getting all faculties");
         model.addAttribute("faculties", facultyService.getAll());
+        log.info("The list of faculties is loaded into the model");
         return "faculty";
     }
 }
