7e9d3cf259372c057ff02d643b6978750d4cdbf6
KaryamaGavi <ii99xt1@gmail.com>
2021-09-11 21:30:48 +0300

    create scripts for manage checkboxes in table and modal remove student from lesson

--- src/main/webapp/WEB-INF/resources/js/checkbox_table.js
+++ src/main/webapp/WEB-INF/resources/js/checkbox_table.js
@@ -1,14 +1,39 @@
 let checkAll = $('#check-all')
-let checkboxes = $('input.checkbox')
+let checkboxes = $('input.checkbox-table')
 
-// function changeStateElementsAfterCheckboxUpdate(){
-//   if ()
-// }
+for (let checkbox of checkboxes) {
+  $(checkbox).on('click', function () {
+    $(this).parents('tr').toggleClass('selected')
+    let checkedCount = $('input.checkbox-table:checked').length
+    checkAll.prop('checked', checkedCount > 0)
+    checkAll.prop('indeterminate', checkedCount > 0 && checkedCount <
+      checkboxes.length)
+    changeAvailabilityElements(checkedCount)
+  })
+}
+
 checkAll.on('click', function () {
-  for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].attr('checked', '')
+  $('.checkbox-table').prop('checked', this.checked)
+  changeAvailabilityElements($('input.checkbox-table:checked').length)
+  if (this.checked === true) {
+    checkboxes.parents('tr').addClass('selected')
+  } else {
+    checkboxes.parents('tr').removeClass('selected')
   }
 })
+
+/**
+ *
+ *
+ * @param checkedCount {Number} - The count checked checkbox
+ */
+function changeAvailabilityElements (checkedCount) {
+  if (checkedCount > 0) {
+    enableElementsAfterCheckboxUpdate()
+  } else {
+    disableElementsAfterCheckboxUpdate()
+  }
+}
 
 function enableElementsAfterCheckboxUpdate () {
   $('.checkbox__depends').removeAttr('disabled')
