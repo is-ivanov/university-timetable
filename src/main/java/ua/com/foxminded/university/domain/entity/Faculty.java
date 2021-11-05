9b28f4b0ee74b61077d4aa9778d3ec61da5734f5
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2021-11-05 14:47:05 +0300

    add hibernate in pom file and config in RootConfig

--- src/main/java/ua/com/foxminded/university/domain/entity/Faculty.java
+++ src/main/java/ua/com/foxminded/university/domain/entity/Faculty.java
@@ -4,12 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "faculties")
 public class Faculty {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "faculty_id")
     private int id;
+
+    @Column(name = "faculty_name")
     private String name;
 
 }
--- src/main/java/ua/com/foxminded/university/springconfig/RootConfig.java
+++ src/main/java/ua/com/foxminded/university/springconfig/RootConfig.java
@@ -6,15 +6,21 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jndi.JndiTemplate;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -22,9 +28,12 @@ import java.util.Objects;
 @ComponentScan({"ua.com.foxminded.university.dao", "ua.com.foxminded.university.domain"})
 public class RootConfig {
 
-    private static final String JDBC_URL = "jdbc.url";
+    public static final String JDBC_URL = "jdbc.url";
+    public static final String PACKAGE_WITH_ENTITY = "ua.com.foxminded.university.domain.entity";
+    public static final String HIBERNATE_HBM_2_DDL_AUTO = "hibernate.hbm2ddl.auto";
+    public static final String HIBERNATE_DIALECT = "hibernate.dialect";
 
-    Environment env;
+    private Environment env;
 
     @Autowired
     public void setEnv(Environment env) {
@@ -37,14 +46,43 @@ public class RootConfig {
             .lookup(Objects.requireNonNull(env.getProperty(JDBC_URL)));
     }
 
-    @Autowired
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+        LocalContainerEntityManagerFactoryBean em =
+            new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(PACKAGE_WITH_ENTITY);
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
 
     @Bean
-    public PlatformTransactionManager txManager() throws NamingException {
-        return new DataSourceTransactionManager(dataSource());
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(emf);
+        return txManager;
     }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(HIBERNATE_HBM_2_DDL_AUTO, env.getProperty(HIBERNATE_HBM_2_DDL_AUTO));
+        properties.setProperty(HIBERNATE_DIALECT, env.getProperty(HIBERNATE_DIALECT));
+
+        return properties;
+    }
+
 }
