d554996146f970f0d538f6a66081de180ed9a845
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2021-11-09 16:59:18 +0300

    refactor Room to @Entity JPA

--- src/main/java/ua/com/foxminded/university/domain/entity/Room.java
+++ src/main/java/ua/com/foxminded/university/domain/entity/Room.java
@@ -1,19 +1,54 @@
 package ua.com.foxminded.university.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "rooms")
 public class Room {
 
-    private int id;
+    @Id
+    @SequenceGenerator(
+        name = "room-id-seq",
+        sequenceName = "rooms_id_seq",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "room-id-seq"
+    )
+    @Column(name = "room_id", nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
     private String building;
+
+    @Column(name = "room_number", nullable = false)
     private String number;
 
     public String getBuildingAndRoom () {
         return this.building + " - " + this.number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+            return false;
+        Room room = (Room) o;
+        return id != null && Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
