7880ba69d1ce88dcf2c9f15b2506e15ef6f3dd3e
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2021-11-18 09:14:00 +0300

    end tests in JpaCourseDaoImplTest

--- src/test/java/ua/com/foxminded/university/dao/jpaimpl/JpaCourseDaoImplTest.java
+++ src/test/java/ua/com/foxminded/university/dao/jpaimpl/JpaCourseDaoImplTest.java
@@ -5,15 +5,21 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
 import ua.com.foxminded.university.dao.interfaces.CourseDao;
 import ua.com.foxminded.university.domain.entity.Course;
+import ua.com.foxminded.university.exception.DaoException;
 import ua.com.foxminded.university.springconfig.IntegrationTestBase;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static ua.com.foxminded.university.TestObjects.*;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
@@ -21,6 +27,8 @@ import static ua.com.foxminded.university.TestObjects.*;
 class JpaCourseDaoImplTest extends IntegrationTestBase {
 
     private static final String TEST_COURSE_NAME = "New course";
+    private static final String MESSAGE_DELETE_COURSE_NOT_FOUND = "Can't delete because course id(4) not found";
+
     @Autowired
     @Qualifier("jpaCourseDaoImpl")
     private CourseDao dao;
@@ -103,132 +111,107 @@ class JpaCourseDaoImplTest extends IntegrationTestBase {
             assertThat(actualCourse).isEqualTo(course);
         }
     }
-//
-//    @Nested
-//    @DisplayName("test 'delete' method")
-//    class DeleteTest {
-//
-//        @Nested
-//        @DisplayName("delete(course) method")
-//        class DeleteCourseTest {
-//
-//            @Test
-//            @DisplayName("with course id=1 should delete one record and number " +
-//                "records table should equals 2")
-//            void testDeleteExistingCourse_ReduceNumberRowsInTable() {
-//                int expectedQuantityCourses = JdbcTestUtils
-//                    .countRowsInTable(jdbcTemplate, TABLE_NAME) - 1;
-//                Course course = new Course(ID1, FIRST_COURSE_NAME);
-//                dao.delete(course);
-//                int actualQuantityCourses = JdbcTestUtils
-//                    .countRowsInTable(jdbcTemplate, TABLE_NAME);
-//                assertEquals(expectedQuantityCourses, actualQuantityCourses);
-//            }
-//
-//            @Test
-//            @DisplayName("with course id=4 should write new log.warn and throw " +
-//                "new DAOException")
-//            void testDeleteNonExistingCourse_ExceptionAndWriteLogWarn() {
-//                LogCaptor logCaptor = LogCaptor.forClass(CourseDaoImpl.class);
-//                Course course = new Course(ID4, TEST_COURSE_NAME);
-//                String expectedLog = String.format(MESSAGE_DELETE_MASK, course);
-//                Exception ex = assertThrows(DaoException.class,
-//                    () -> dao.delete(course));
-//                assertEquals(expectedLog, logCaptor.getWarnLogs().get(0));
-//                assertEquals(MESSAGE_DELETE_EXCEPTION, ex.getMessage());
-//            }
-//        }
-//
-//        @Nested
-//        @DisplayName("delete(courseId) method")
-//        class DeleteCourseIdTest {
-//
-//            @Test
-//            @DisplayName("with course id=1 should delete one record and number " +
-//                "records table should equals 2")
-//            void testDeleteExistingCourseId1_ReduceNumberRowsInTable() {
-//                int expectedQuantityCourses = JdbcTestUtils
-//                    .countRowsInTable(jdbcTemplate, TABLE_NAME) - 1;
-//                dao.delete(ID1);
-//                int actualQuantityCourses = JdbcTestUtils
-//                    .countRowsInTable(jdbcTemplate, TABLE_NAME);
-//                assertEquals(expectedQuantityCourses, actualQuantityCourses);
-//            }
-//
-//            @Test
-//            @DisplayName("with course id=4 should write new log.warn and throw " +
-//                "new DAOException")
-//            void testDeleteNonExistingCourse_ExceptionAndWriteLogWarn() {
-//                LogCaptor logCaptor = LogCaptor.forClass(CourseDaoImpl.class);
-//                String expectedLog = String.format(MESSAGE_DELETE_ID_MASK, ID4);
-//                Exception ex = assertThrows(DaoException.class,
-//                    () -> dao.delete(ID4));
-//                assertEquals(expectedLog, logCaptor.getWarnLogs().get(0));
-//                assertEquals(MESSAGE_DELETE_EXCEPTION, ex.getMessage());
-//            }
-//        }
-//
-//    }
-//
-//    @Nested
-//    @DisplayName("test 'countAll' method")
-//    class CountAllTest {
-//
-//        @Test
-//        @DisplayName("should return number 3")
-//        void testReturnCountAllRecords() {
-//            int expectedRows = JdbcTestUtils.countRowsInTable(jdbcTemplate, TABLE_NAME);
-//            assertThat(dao.countAll(), is(equalTo(expectedRows)));
-//
-//        }
-//    }
-//
-//    @Nested
-//    @DisplayName("test 'getAllSortedPaginated' method")
-//    class GetAllSortedPaginatedTest {
-//
-//        long totalCourses = 3L;
-//
-//        @Test
-//        @DisplayName("when pageable size = 1 without sort then return page with first record sorted by course name")
-//        void test_Size1WithoutSorting() {
-//            int pageNumber = 0;
-//            int pageSize = 1;
-//            Pageable pageable = PageRequest.of(pageNumber, pageSize);
-//
-//            Page<Course> page = dao.getAllSortedPaginated(pageable);
-//
-//            assertThat(page.getSize(), is(equalTo(pageSize)));
-//            assertThat(page.getTotalElements(), is(equalTo(totalCourses)));
-//            assertThat(page.getNumber(), is(equalTo(pageNumber)));
-//
-//            Course course = page.getContent().get(0);
-//            assertThat(course.getId(), is(equalTo(ID3)));
-//            assertThat(course.getName(), is(equalTo(THIRD_COURSE_NAME)));
-//        }
-//
-//        @Test
-//        @DisplayName("when pageable with sort by ID then return sorted by ID page")
-//        void test_Size2WithSortingById() {
-//            int pageNumber = 0;
-//            int pageSize = 2;
-//            Sort sort = Sort.by(Sort.Direction.ASC, "course_id");
-//            Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
-//
-//            Page<Course> page = dao.getAllSortedPaginated(pageable);
-//
-//            assertThat(page.getSize(), is(equalTo(pageSize)));
-//            assertThat(page.getTotalElements(), is(equalTo(totalCourses)));
-//            assertThat(page.getNumber(), is(equalTo(pageNumber)));
-//
-//            List<Course> courses = page.getContent();
-//            Course course1 = courses.get(0);
-//            Course course2 = courses.get(1);
-//
-//            assertThat(course1.getId(), is(equalTo(ID1)));
-//            assertThat(course2.getId(), is(equalTo(ID2)));
-//            assertThat(course1.getName(), is(equalTo(FIRST_COURSE_NAME)));
-//            assertThat(course2.getName(), is(equalTo(SECOND_COURSE_NAME)));
-//        }
-//    }
+
+    @Nested
+    @DisplayName("test 'delete' method")
+    class DeleteTest {
+
+        @Nested
+        @DisplayName("delete(course) method")
+        class DeleteCourseTest {
+
+            @Test
+            @DisplayName("with course id=1 should delete one record and number " +
+                "records table should equals 2")
+            void testDeleteExistingCourse_ReduceNumberRowsInTable() {
+                int expectedQuantityCourses = dao.countAll() - 1;
+                Course course = dao.getById(ID1).get();
+                dao.delete(course);
+                int actualQuantityCourses = dao.countAll();
+                assertThat(actualQuantityCourses).isEqualTo(expectedQuantityCourses);
+            }
+        }
+
+        @Nested
+        @DisplayName("delete(courseId) method")
+        class DeleteCourseIdTest {
+
+            @Test
+            @DisplayName("with course id=1 should delete one record and number " +
+                "records table should equals 2")
+            void testDeleteExistingCourseId1_ReduceNumberRowsInTable() {
+                int expectedQuantityCourses = dao.countAll() - 1;
+                dao.delete(ID1);
+                int actualQuantityCourses = dao.countAll();
+                assertThat(actualQuantityCourses).isEqualTo(expectedQuantityCourses);
+            }
+
+            @Test
+            @DisplayName("with course id=4 should write new log.warn and throw " +
+                "new DAOException")
+            void testDeleteNonExistingCourse_ExceptionAndWriteLogWarn() {
+                assertThatThrownBy(() -> dao.delete(4))
+                    .isInstanceOf(DaoException.class)
+                    .hasMessageContaining(MESSAGE_DELETE_COURSE_NOT_FOUND);
+            }
+        }
+
+    }
+
+    @Nested
+    @DisplayName("test 'countAll' method")
+    class CountAllTest {
+
+        @Test
+        @DisplayName("should return number 3")
+        void testReturnCountAllRecords() {
+            assertThat(dao.countAll()).isEqualTo(3);
+        }
+    }
+
+    @Nested
+    @DisplayName("test 'getAllSortedPaginated' method")
+    class GetAllSortedPaginatedTest {
+
+        long totalCourses = 3L;
+
+        @Test
+        @DisplayName("when pageable size = 1 without sort then return page with " +
+            "first record sorted by course name")
+        void test_Size1WithoutSorting() {
+            int pageNumber = 0;
+            int pageSize = 1;
+            Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+            Page<Course> page = dao.getAllSortedPaginated(pageable);
+
+            assertThat(page.getSize()).isEqualTo(pageSize);
+            assertThat(page.getTotalElements()).isEqualTo(totalCourses);
+            assertThat(page.getNumber()).isEqualTo(pageNumber);
+
+            Course course = page.getContent().get(0);
+            assertThat(course.getId()).isEqualTo(ID3);
+            assertThat(course.getName()).isEqualTo(NAME_THIRD_COURSE);
+        }
+
+        @Test
+        @DisplayName("when pageable with sort by ID then return sorted by ID page")
+        void test_Size2WithSortingById() {
+            int pageNumber = 0;
+            int pageSize = 2;
+            Sort sort = Sort.by(Sort.Direction.ASC, "course_id");
+            Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+
+            Page<Course> page = dao.getAllSortedPaginated(pageable);
+
+            assertThat(page.getSize()).isEqualTo(pageSize);
+            assertThat(page.getTotalElements()).isEqualTo(totalCourses);
+            assertThat(page.getNumber()).isEqualTo(pageNumber);
+
+            List<Course> courses = page.getContent();
+
+            assertThat(courses).extracting(Course::getId)
+                .containsExactly(ID1, ID2);
+        }
+    }
 }
