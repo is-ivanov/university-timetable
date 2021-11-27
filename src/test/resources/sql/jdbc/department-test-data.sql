4334e4bce8f2d07845773b4620cc3f042509bd2d
KaryamaGavi <ii99xt1@gmail.com>
2021-11-27 23:07:09 +0300

    delete Dao with JdbcTemplate realization

--- src/test/resources/sql/jdbc/department-test-data.sql
+++
@@ -1,5 +0,0 @@
-insert into faculties (faculty_name) values ('Chemical Technology');
-
-insert into departments (department_name, faculty_id) values ('Chemistry', 1);
-insert into departments (department_name, faculty_id) values ('Oil Technology', 1);
-
--- src/test/resources/sql/jdbc/room-test-data.sql
+++
@@ -1,3 +0,0 @@
-insert into rooms (building, room_number) values ('building-1', '1457a');
-insert into rooms (building, room_number) values ('building-1', '812b');
-insert into rooms (building, room_number) values ('building-2', '145');
