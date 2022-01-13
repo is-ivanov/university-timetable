13bab91bfd8a7d7bd25d444b93142276aa203ee6
KaryamaGavi <ii99xt1@gmail.com>
2022-01-14 00:51:42 +0300

    refactor FacultyModelAssembler

--- src/main/java/ua/com/foxminded/university/ui/restcontroller/link/FacultyModelAssembler.java
+++ src/main/java/ua/com/foxminded/university/ui/restcontroller/link/FacultyModelAssembler.java
@@ -1,24 +1,30 @@
 package ua.com.foxminded.university.ui.restcontroller.link;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 import ua.com.foxminded.university.domain.dto.FacultyDto;
-import ua.com.foxminded.university.ui.restcontroller.FacultyRestController;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class FacultyModelAssembler implements RepresentationModelAssembler<FacultyDto,
-    EntityModel<FacultyDto>> {
-
-    @SuppressWarnings("NullableProblems")
+public class FacultyModelAssembler implements SimpleRepresentationModelAssembler<FacultyDto> {
     @Override
-    public EntityModel<FacultyDto> toModel(FacultyDto faculty) {
-        return EntityModel.of(faculty,
-            linkTo(methodOn(FacultyRestController.class).getFaculty(faculty.getId())).withSelfRel(),
-            linkTo(FacultyRestController.class).withRel("faculties"));
+    public void addLinks(EntityModel<FacultyDto> resource) {
+
     }
+
+    @Override
+    public void addLinks(CollectionModel<EntityModel<FacultyDto>> resources) {
+
+    }
+
+//    @SuppressWarnings("NullableProblems")
+//    @Override
+//    public EntityModel<FacultyDto> toModel(FacultyDto faculty) {
+//        return EntityModel.of(faculty,
+//            linkTo(methodOn(FacultyRestController.class).getFaculty(faculty.getId())).withSelfRel(),
+//            linkTo(FacultyRestController.class).withRel("faculties"));
+//    }
+
 
 }
