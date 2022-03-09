9b3f659ff4c849a31a6f6961ee6fc031cb14f8ec
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2022-03-09 15:42:43 +0300

    correct some tests in LessonControllerTest and create LessonRestControllerTest

--- src/test/java/ua/com/foxminded/university/ui/controller/LessonControllerTest.java
+++ src/test/java/ua/com/foxminded/university/ui/controller/LessonControllerTest.java
@@ -1,198 +1,190 @@
-//package ua.com.foxminded.university.ui.controller;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import ua.com.foxminded.university.domain.dto.DepartmentDto;
-//import ua.com.foxminded.university.domain.dto.GroupDto;
-//import ua.com.foxminded.university.domain.dto.LessonDto;
-//import ua.com.foxminded.university.domain.dto.TeacherDto;
-//import ua.com.foxminded.university.domain.entity.Course;
-//import ua.com.foxminded.university.domain.entity.Faculty;
-//import ua.com.foxminded.university.domain.entity.Lesson;
-//import ua.com.foxminded.university.domain.entity.Room;
-//import ua.com.foxminded.university.domain.filter.LessonFilter;
-//import ua.com.foxminded.university.domain.mapper.LessonDtoMapper;
-//import ua.com.foxminded.university.domain.service.interfaces.*;
-//
-//import java.util.List;
-//
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.hamcrest.Matchers.is;
-//import static org.mockito.Mockito.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//import static ua.com.foxminded.university.TestObjects.*;
-//import static ua.com.foxminded.university.ui.controller.GroupControllerTest.ON;
-//
-//@ExtendWith(MockitoExtension.class)
-//class LessonControllerTest {
-//
-//    public static final String URI_LESSONS = "/lessons";
-//    public static final String URI_LESSONS_ID = "/lessons/{lessonId}";
-//    public static final String URI_LESSONS_FILTER = "/lessons/filter";
-//    public static final String URI_LESSONS_ID_STUDENTS = "/lessons/{lessonId}/students";
-//    public static final String URI_LESSONS_ID_GROUPS = "/lessons/{lessonId}/groups";
-//
-//    private MockMvc mockMvc;
-//
-//    @Mock
-//    private LessonService lessonServiceMock;
-//
-//    @Mock
-//    private FacultyService facultyServiceMock;
-//
-//    @Mock
-//    private TeacherService teacherServiceMock;
-//
-//    @Mock
-//    private DepartmentService departmentServiceMock;
-//
-//    @Mock
-//    private CourseService courseServiceMock;
-//
-//    @Mock
-//    private RoomService roomServiceMock;
-//
-//    @Mock
-//    private GroupService groupServiceMock;
-//
-//    @Mock
-//    private LessonDtoMapper lessonDtoMapperMock;
-//
-//    @InjectMocks
-//    private LessonController lessonController;
-//
-//    @BeforeEach
-//    void setUp() {
-//        mockMvc = MockMvcBuilders
-//            .standaloneSetup(lessonController)
-//            .build();
-//    }
-//
-//
-//    @Nested
-//    @DisplayName("test 'showLessons' method")
-//    class ShowLessonsTest {
-//
-//        @Test
-//        @DisplayName("when GET request without parameters then should load into " +
-//            "model expected data")
-//        void getRequestWithoutParameters() throws Exception {
-//            List<Faculty> faculties = createTestFaculties();
-//            List<DepartmentDto> departments = createTestDepartmentDtos();
-//            List<TeacherDto> teacherDtos = createTestTeacherDtos(ID1);
-//            List<Course> courses = createTestCourses();
-//            List<Room> rooms = createTestRooms();
-//
-//            when(facultyServiceMock.getAllSortedByNameAsc()).thenReturn(faculties);
-//            when(departmentServiceMock.getAll()).thenReturn(departments);
-//            when(teacherServiceMock.getAll()).thenReturn(teacherDtos);
-//            when(courseServiceMock.getAll()).thenReturn(courses);
-//            when(roomServiceMock.getAll()).thenReturn(rooms);
-//
-//            mockMvc.perform(get(URI_LESSONS))
-//                .andDo(print())
-//                .andExpectAll(
-//                    status().isOk(),
-//                    view().name("all_lessons"),
-//                    model().attributeDoesNotExist("isShowInactiveTeachers",
-//                        "isShowPastLessons", "lessons"),
-//                    model().attribute("lessonFilter", new LessonFilter()),
-//                    model().attribute("faculties", faculties),
-//                    model().attribute("departments", departments),
-//                    model().attribute("teachers", teacherDtos),
-//                    model().attribute("courses", courses),
-//                    model().attribute("rooms", rooms)
-//                );
-//        }
-//    }
-//
-//    @Nested
-//    @DisplayName("test 'showFilteredLesson' method")
-//    class ShowFilteredLessonTest {
-//
-//        @Test
-//        @DisplayName("when GET request with all parameters then should call " +
-//            "expected methods in services")
-//        void getRequestWithAllParameters() throws Exception {
-//            LessonFilter lessonFilter = createTestLessonFilter();
-//
-//            List<TeacherDto> teachers = createTestTeacherDtos(FACULTY_ID1);
-//            when(teacherServiceMock.getAllByDepartment(DEPARTMENT_ID1))
-//                .thenReturn(teachers);
-//
-//            List<DepartmentDto> departments = createTestDepartmentDtos();
-//            when(departmentServiceMock.getAllByFaculty(FACULTY_ID1))
-//                .thenReturn(departments);
-//
-//            List<LessonDto> testLessonDtos = createTestLessonDtos();
-//            when(lessonServiceMock.getAllWithFilter(lessonFilter))
-//                .thenReturn(testLessonDtos);
-//
-//
-//            mockMvc.perform(get(URI_LESSONS_FILTER)
-//                    .param("facultyId", String.valueOf(FACULTY_ID1))
-//                    .param("departmentId", String.valueOf(DEPARTMENT_ID1))
-//                    .param("teacherId", String.valueOf(TEACHER_ID1))
-//                    .param("courseId", String.valueOf(COURSE_ID1))
-//                    .param("roomId", String.valueOf(ROOM_ID1))
-//                    .param("dateFrom", TEXT_DATE_FROM)
-//                    .param("dateTo", TEXT_DATE_TO)
-//                    .param("isShowInactiveTeachers", ON)
-//                    .param("isShowPastLessons", ON))
-//                .andDo(print())
-//                .andExpectAll(
-//                    status().isOk(),
-//                    view().name("all_lessons"),
-//                    model().attribute("isShowInactiveTeachers", true),
-//                    model().attribute("isShowPastLessons", true),
-//                    model().attribute("departments", departments)
-//                );
-//
-//            verify(lessonServiceMock, times(1)).getAllWithFilter(lessonFilter);
-//            verify(teacherServiceMock, times(0)).getAllByFaculty(anyInt());
-//        }
-//
-//        @Test
-//        @DisplayName("when GET request with parameter facultyId and without " +
-//            "departmentId then should call teacherService.getAllByDepartment")
-//        void getRequestWithFacultyIdAndWithoutDepartmentId() throws Exception {
-//            int facultyId = 3;
-//            LessonFilter lessonFilter = LessonFilter.builder()
-//                .facultyId(facultyId)
-//                .build();
-//
-//            mockMvc.perform(get(URI_LESSONS_FILTER)
-//                    .param("facultyId", String.valueOf(facultyId)))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//
-//            verify(lessonServiceMock, times(1)).getAllWithFilter(lessonFilter);
-//            verify(teacherServiceMock, times(1)).getAllByFaculty(facultyId);
-//        }
-//
-//        @Test
-//        @DisplayName("when GET request without parameters departmentId and facultyId " +
-//            "then should call departmentService.getAll and teacherService.getAll")
-//        void getRequestWithoutParametersFacultyIdAndDepartmentId() throws Exception {
-//            mockMvc.perform(get(URI_LESSONS_FILTER))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//            verify(departmentServiceMock, times(2)).getAll();
-//            verify(teacherServiceMock, times(2)).getAll();
-//        }
-//
-//    }
+package ua.com.foxminded.university.ui.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ua.com.foxminded.university.domain.entity.*;
+import ua.com.foxminded.university.domain.filter.LessonFilter;
+import ua.com.foxminded.university.domain.mapper.LessonDtoMapper;
+import ua.com.foxminded.university.domain.service.interfaces.*;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ua.com.foxminded.university.TestObjects.*;
+import static ua.com.foxminded.university.ui.controller.GroupControllerTest.ON;
+
+@ExtendWith(MockitoExtension.class)
+class LessonControllerTest {
+
+    public static final String URI_LESSONS = "/lessons";
+    public static final String URI_LESSONS_ID = "/lessons/{lessonId}";
+    public static final String URI_LESSONS_FILTER = "/lessons/filter";
+    public static final String URI_LESSONS_ID_STUDENTS = "/lessons/{lessonId}/students";
+    public static final String URI_LESSONS_ID_GROUPS = "/lessons/{lessonId}/groups";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private LessonService lessonServiceMock;
+
+    @Mock
+    private FacultyService facultyServiceMock;
+
+    @Mock
+    private TeacherService teacherServiceMock;
+
+    @Mock
+    private DepartmentService departmentServiceMock;
+
+    @Mock
+    private CourseService courseServiceMock;
+
+    @Mock
+    private RoomService roomServiceMock;
+
+    @Mock
+    private GroupService groupServiceMock;
+
+    @Mock
+    private LessonDtoMapper lessonDtoMapperMock;
+
+    @InjectMocks
+    private LessonController lessonController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+            .standaloneSetup(lessonController)
+            .build();
+    }
+
+
+    @Nested
+    @DisplayName("test 'showLessons' method")
+    class ShowLessonsTest {
+
+        @Test
+        @DisplayName("when GET request without parameters then should load into " +
+            "model expected data")
+        void getRequestWithoutParameters() throws Exception {
+            List<Faculty> faculties = createTestFaculties();
+            List<Department> departments = createTestDepartments();
+            List<Teacher> teacherDtos = createTestTeachers(ID1);
+            List<Course> courses = createTestCourses();
+            List<Room> rooms = createTestRooms();
+
+
+            when(facultyServiceMock.getAllSortedByNameAsc()).thenReturn(faculties);
+            when(departmentServiceMock.findAll()).thenReturn(departments);
+            when(teacherServiceMock.findAll()).thenReturn(teacherDtos);
+            when(courseServiceMock.findAll()).thenReturn(courses);
+            when(roomServiceMock.findAll()).thenReturn(rooms);
+
+            mockMvc.perform(get(URI_LESSONS))
+                .andDo(print())
+                .andExpectAll(
+                    status().isOk(),
+                    view().name("all_lessons"),
+                    model().attributeDoesNotExist("isShowInactiveTeachers",
+                        "isShowPastLessons", "lessons"),
+                    model().attribute("lessonFilter", new LessonFilter()),
+                    model().attribute("faculties", faculties),
+                    model().attribute("departments", departments),
+                    model().attribute("teachers", teacherDtos),
+                    model().attribute("courses", courses),
+                    model().attribute("rooms", rooms)
+                );
+        }
+    }
+
+    @Nested
+    @DisplayName("test 'showFilteredLesson' method")
+    class ShowFilteredLessonTest {
+
+        @Test
+        @DisplayName("when GET request with all parameters then should call " +
+            "expected methods in services")
+        void getRequestWithAllParameters() throws Exception {
+            LessonFilter lessonFilter = createTestLessonFilter();
+
+            List<Teacher> teachers = createTestTeachers(FACULTY_ID1);
+            when(teacherServiceMock.getAllByDepartment(DEPARTMENT_ID1))
+                .thenReturn(teachers);
+
+            List<Department> departments = createTestDepartments();
+            when(departmentServiceMock.getAllByFaculty(FACULTY_ID1))
+                .thenReturn(departments);
+
+            List<Lesson> lessons = createTestLessons();
+            when(lessonServiceMock.getAllWithFilter(lessonFilter))
+                .thenReturn(lessons);
+
+
+            mockMvc.perform(get(URI_LESSONS_FILTER)
+                    .param("facultyId", String.valueOf(FACULTY_ID1))
+                    .param("departmentId", String.valueOf(DEPARTMENT_ID1))
+                    .param("teacherId", String.valueOf(TEACHER_ID1))
+                    .param("courseId", String.valueOf(COURSE_ID1))
+                    .param("roomId", String.valueOf(ROOM_ID1))
+                    .param("dateFrom", TEXT_DATE_FROM)
+                    .param("dateTo", TEXT_DATE_TO)
+                    .param("isShowInactiveTeachers", ON)
+                    .param("isShowPastLessons", ON))
+                .andDo(print())
+                .andExpectAll(
+                    status().isOk(),
+                    view().name("all_lessons"),
+                    model().attribute("isShowInactiveTeachers", true),
+                    model().attribute("isShowPastLessons", true),
+                    model().attribute("departments", departments)
+                );
+
+            verify(lessonServiceMock, times(1)).getAllWithFilter(lessonFilter);
+            verify(teacherServiceMock, times(0)).getAllByFaculty(anyInt());
+        }
+
+        @Test
+        @DisplayName("when GET request with parameter facultyId and without " +
+            "departmentId then should call teacherService.getAllByFaculty")
+        void getRequestWithFacultyIdAndWithoutDepartmentId() throws Exception {
+            int facultyId = 3;
+            LessonFilter lessonFilter = LessonFilter.builder()
+                .facultyId(facultyId)
+                .build();
+
+            mockMvc.perform(get(URI_LESSONS_FILTER)
+                    .param("facultyId", String.valueOf(facultyId)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+            verify(lessonServiceMock, times(1)).getAllWithFilter(lessonFilter);
+            verify(teacherServiceMock, times(1)).getAllByFaculty(facultyId);
+        }
+
+        @Test
+        @DisplayName("when GET request without parameters departmentId and facultyId " +
+            "then should call departmentService.getAll and teacherService.findAll")
+        void getRequestWithoutParametersFacultyIdAndDepartmentId() throws Exception {
+            mockMvc.perform(get(URI_LESSONS_FILTER))
+                .andDo(print())
+                .andExpect(status().isOk());
+            verify(departmentServiceMock, times(2)).findAll();
+            verify(teacherServiceMock, times(2)).findAll();
+        }
+
+    }
+
 //
 //    @Nested
 //    @DisplayName("test 'getLessonWithStudents' method")
@@ -427,4 +419,4 @@
 //            verify(lessonServiceMock, times(1)).delete(lessonId);
 //        }
 //    }
-//}
+}
---
+++ src/test/java/ua/com/foxminded/university/ui/restcontroller/LessonRestControllerTest.java
@@ -0,0 +1,9 @@
+package ua.com.foxminded.university.ui.restcontroller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LessonRestControllerTest {
+
+
+
+}
