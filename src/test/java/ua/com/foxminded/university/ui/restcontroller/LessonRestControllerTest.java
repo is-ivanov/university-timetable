534738c12380f0cde5f17fa7381b3bec37e1010b
ivan <i.s.ivanov2307@gmail.com>
2022-03-10 20:55:49 +0300

    correct test in LessonRestControllerTest

--- src/test/java/ua/com/foxminded/university/ui/restcontroller/LessonRestControllerTest.java
+++ src/test/java/ua/com/foxminded/university/ui/restcontroller/LessonRestControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.com.foxminded.university.domain.dto.LessonDto;
 import ua.com.foxminded.university.domain.entity.*;
 import ua.com.foxminded.university.domain.service.interfaces.LessonService;
 import ua.com.foxminded.university.springconfig.TestMapperConfig;
@@ -21,8 +22,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ua.com.foxminded.university.TestObjects.*;
@@ -134,34 +134,56 @@ class LessonRestControllerTest {
         }
     }
 
-//    @Nested
-//    @DisplayName("test 'updateLesson' method")
-//    class UpdateLessonTest {
-//
-//        @Test
-//        @DisplayName("when PUT request with all required parameters then should " +
-//            "call lessonService.update")
-//        void putRequestWithAllParametersShouldCallLessonServiceAndRedirect() throws Exception {
-//            int lessonId = 45;
-//            LessonDto testLessonDto = LessonDto.builder()
-//                .id(lessonId)
-//                .courseId(COURSE_ID1)
-//                .teacherId(TEACHER_ID1)
-//                .roomId(ROOM_ID1)
-//                .timeStart(DATE_START_FIRST_LESSON)
-//                .timeEnd(DATE_END_FIRST_LESSON)
-//                .build();
-//
-//            mockMvc.perform(put(URI_LESSONS_ID, lessonId)
-//                    .param("courseId", String.valueOf(COURSE_ID1))
-//                    .param("teacherId", String.valueOf(TEACHER_ID1))
-//                    .param("roomId", String.valueOf(ROOM_ID1))
-//                    .param("timeStart", TEXT_DATE_START_FIRST_LESSON)
-//                    .param("timeEnd", TEXT_DATE_END_FIRST_LESSON))
-//                .andDo(print())
-//                .andExpect(status().is2xxSuccessful());
-//
-//            verify(lessonServiceMock, times(1)).update(testLessonDto);
-//        }
-//    }
+    @Nested
+    @DisplayName("test 'updateLesson' method")
+    class UpdateLessonTest {
+
+        @Test
+        @DisplayName("when PUT request with all required parameters then should " +
+            "call lessonService.update")
+        void putRequestWithAllParametersShouldCallLessonServiceAndRedirect() throws Exception {
+            int lessonId = 45;
+
+            String bodyRequest = "{" +
+                "    \"id\": " + lessonId + "," +
+                "    \"courseId\": 21," +
+                "    \"teacherId\": 78," +
+                "    \"roomId\": 123," +
+                "    \"timeStart\": \"2022-02-03T15:00\"," +
+                "    \"timeEnd\": \"2022-02-03T16:30\"" +
+                "}";
+
+            Lesson lessonAfterUpdating = createTestLesson(ID1);
+
+            when(lessonServiceMock.update(anyInt(), any(Lesson.class)))
+                .thenReturn(lessonAfterUpdating);
+
+            mockMvc.perform(put(API_LESSONS_ID, lessonId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(bodyRequest))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+            verify(lessonServiceMock, times(1))
+                .update(eq(lessonId), lessonCapture.capture());
+
+            Lesson savingLesson = lessonCapture.getValue();
+            LessonAssert.assertThat(savingLesson)
+                .hasId(lessonId)
+                .hasTimeStart(LocalDateTime.of(2022, 2, 3, 15, 0))
+                .hasTimeEnd(LocalDateTime.of(2022, 2, 3, 16, 30));
+            CourseAssert.assertThat(savingLesson.getCourse())
+                .hasId(21)
+                .hasName(null);
+            TeacherAssert.assertThat(savingLesson.getTeacher())
+                .hasId(78)
+                .hasFirstName(null)
+                .hasLastName(null)
+                .hasDepartment(null);
+            RoomAssert.assertThat(savingLesson.getRoom())
+                .hasId(123)
+                .hasBuilding(null)
+                .hasNumber(null);
+        }
+    }
 }
