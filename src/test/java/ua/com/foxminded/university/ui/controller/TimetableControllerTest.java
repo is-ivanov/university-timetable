16549381e0414a20b1bdd8633966f4c4f4cafe9c
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2022-03-14 14:33:11 +0300

    correct TimetableControllerTest

--- src/test/java/ua/com/foxminded/university/ui/controller/TimetableControllerTest.java
+++ src/test/java/ua/com/foxminded/university/ui/controller/TimetableControllerTest.java
@@ -11,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ua.com.foxminded.university.domain.entity.Room;
+import ua.com.foxminded.university.domain.entity.Student;
+import ua.com.foxminded.university.domain.entity.Teacher;
 import ua.com.foxminded.university.domain.service.interfaces.RoomService;
 import ua.com.foxminded.university.domain.service.interfaces.StudentService;
 import ua.com.foxminded.university.domain.service.interfaces.TeacherService;
@@ -19,7 +21,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ua.com.foxminded.university.TestObjects.createTestRoom;
+import static ua.com.foxminded.university.TestObjects.*;
 import static ua.com.foxminded.university.ui.controller.TimetableController.TIMETABLE_TEMPLATE;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,49 +55,51 @@ class TimetableControllerTest {
     @Nested
     @DisplayName("test 'showStudentTimetable' method")
     class ShowStudentTimetableTest {
-//        @Test
-//        @DisplayName("when GET request with PathVariable id then should call " +
-//            "expected services and return template")
-//        void getRequestWithPathVariableIdShouldCallServicesAndReturnTemplate() throws Exception {
-//            int studentId = 23;
-//
-//            StudentDto testStudentDto = createTestStudentDto();
-//
-//            when(studentServiceMock.getById(studentId)).thenReturn(testStudentDto);
-//
-//            mockMvc.perform(get(URI_TIMETABLE_STUDENT, studentId))
-//                .andDo(print())
-//                .andExpectAll(
-//                    status().isOk(),
-//                    view().name(TIMETABLE_TEMPLATE),
-//                    model().attribute("type", "students"),
-//                    model().attribute("object", testStudentDto)
-//                );
-//        }
+        @Test
+        @DisplayName("when GET request with PathVariable id then should call " +
+            "expected services and return template")
+        void getRequestWithPathVariableIdShouldCallServicesAndReturnTemplate()
+            throws Exception {
+            int studentId = 23;
+
+            Student student = createTestStudent();
+
+            when(studentServiceMock.findById(studentId)).thenReturn(student);
+
+            mockMvc.perform(get(URI_TIMETABLE_STUDENT, studentId))
+                .andDo(print())
+                .andExpectAll(
+                    status().isOk(),
+                    view().name(TIMETABLE_TEMPLATE),
+                    model().attribute("type", "students"),
+                    model().attribute("object", student)
+                );
+        }
     }
 
     @Nested
     @DisplayName("test 'showTeacherTimetable' method")
     class ShowTeacherTimetableTest {
-//        @Test
-//        @DisplayName("when GET request with PathVariable id then should call " +
-//            "expected services and return template")
-//        void getRequestWithPathVariableIdShouldCallServicesAndReturnTemplate() throws Exception {
-//            int teacherId = 89;
-//
-//            TeacherDto testTeacherDto = createTestTeacherDto();
-//
-//            when(teacherServiceMock.getById(teacherId)).thenReturn(testTeacherDto);
-//
-//            mockMvc.perform(get(URI_TIMETABLE_TEACHER, teacherId))
-//                .andDo(print())
-//                .andExpectAll(
-//                    status().isOk(),
-//                    view().name(TIMETABLE_TEMPLATE),
-//                    model().attribute("type", "teachers"),
-//                    model().attribute("object", testTeacherDto)
-//                );
-//        }
+        @Test
+        @DisplayName("when GET request with PathVariable id then should call " +
+            "expected services and return template")
+        void getRequestWithPathVariableIdShouldCallServicesAndReturnTemplate()
+            throws Exception {
+            int teacherId = 89;
+
+            Teacher teacher = createTestTeacher();
+
+            when(teacherServiceMock.findById(teacherId)).thenReturn(teacher);
+
+            mockMvc.perform(get(URI_TIMETABLE_TEACHER, teacherId))
+                .andDo(print())
+                .andExpectAll(
+                    status().isOk(),
+                    view().name(TIMETABLE_TEMPLATE),
+                    model().attribute("type", "teachers"),
+                    model().attribute("object", teacher)
+                );
+        }
     }
 
     @Nested
@@ -104,12 +108,13 @@ class TimetableControllerTest {
         @Test
         @DisplayName("when GET request with PathVariable id then should call " +
             "expected services and return template")
-        void getRequestWithPathVariableIdShouldCallServicesAndReturnTemplate() throws Exception {
+        void getRequestWithPathVariableIdShouldCallServicesAndReturnTemplate()
+            throws Exception {
             int roomId = 3;
 
-            Room testRoom = createTestRoom();
+            Room room = createTestRoom();
 
-            when(roomServiceMock.findById(roomId)).thenReturn(testRoom);
+            when(roomServiceMock.findById(roomId)).thenReturn(room);
 
             mockMvc.perform(get(URI_TIMETABLE_ROOM, roomId))
                 .andDo(print())
@@ -117,7 +122,7 @@ class TimetableControllerTest {
                     status().isOk(),
                     view().name(TIMETABLE_TEMPLATE),
                     model().attribute("type", "rooms"),
-                    model().attribute("object", testRoom)
+                    model().attribute("object", room)
                 );
         }
     }
